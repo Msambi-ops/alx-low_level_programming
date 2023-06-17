d965417282dc6111ebbfdb8aeb024b2776ffe8e5
Msambi-ops <msambiroman@gmail.com>
2023-06-16 20:03:39 -0700

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/**
- * main - Entry point.
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	for (int i = 0; i <= 9; i++)
-	{
-		putchar(i + '0');
-		if (i == 9)
-		{
-			break;
-		}
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-	return (0);#include <stdio.h>
 
 /**
  * main - main block
