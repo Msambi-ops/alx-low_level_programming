cfce7fe2a8bc57b1ed2547b968f2a12c90ee167c
Msambi-ops <msambiroman@gmail.com>
2023-06-16 21:34:52 -0700

    print combination 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,9 @@ int main(void)
 	int i, j, k;
 	for (i = 0; i <= 9; i++)
 	{
-		for (int j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			for (int k = j + 1; k <= 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				if (i == j || j == k || i == k)
 				{
