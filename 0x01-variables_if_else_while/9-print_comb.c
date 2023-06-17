1c57ff10e3f46fb6a0b053d1d3b621a2a9ef8c52
Msambi-ops <msambiroman@gmail.com>
2023-06-16 19:58:03 -0700

    print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i == 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+}
