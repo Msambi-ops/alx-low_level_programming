2edb76e1d2514dc16dadcf0280b65c09fe59ab58
Msambi-ops <msambiroman@gmail.com>
2023-06-16 21:24:46 -0700

    print comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = i + 1; j <= 9; j++)
+		{
+			for (int k = j + 1; k <= 9; k++)
+			{
+				if (i == j || j == k || i == k)
+				{
+					continue;
+				}
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+			       	if (i == 7 && j == 8 && k == 9)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
