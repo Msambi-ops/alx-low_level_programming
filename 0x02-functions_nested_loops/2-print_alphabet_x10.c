1084489fd7b9a5cd287ef79333fa96059326d438
Msambi-ops <msambiroman@gmail.com>
2023-06-20 13:23:40 -0700

    print times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		putchar('\n');
+	}
+}
