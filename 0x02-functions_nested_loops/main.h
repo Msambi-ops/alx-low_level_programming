a51e8c0bb6b56db425a84e2aa37f915054db40e7
Msambi-ops <msambiroman@gmail.com>
2023-06-20 09:55:45 -0700

    print _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int function1(int arg1, int arg2);
-void function2(char *str);
-int function3(int arg1, char *str);
-int _putchar(char c);
+int prnt(char);
 
-#endif /* MAIN_H */
+#endif
