0293ec7c8ebf2b50933cf076406f090ddfdd880a
Msambi-ops <msambiroman@gmail.com>
2023-06-20 17:51:05 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
 /**
- *print_last_digit - prints the last digit of a number
- *@num: type integer
+ *print_last_digit - prints the last digit
+ *@q: type integer
  *Return: The value of the last digit
  */
 
-int print_last_digit(int num)
+int print_last_digit(int q)
 {
-	int last_digit = num % 10;
+	int last_digit;
 
-	_putchar("The last digit of %d is %d\n", num, last_digit);
+	last_digit = q % 10;
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
