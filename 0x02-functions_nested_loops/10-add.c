4136779b17699f9c4c51cda40f34d4fd933a53d8
Msambi-ops <msambiroman@gmail.com>
2023-06-20 18:57:07 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @x: int type number
+ * @m: int type number
+ * Return: addition result
+ */
+
+int add(int x, int m)
+{
+	return (x + m);
+}
