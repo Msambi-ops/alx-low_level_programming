d5f6a43ef6431e5a901f9128e2115bdde24838c8
Msambi-ops <msambiroman@gmail.com>
2023-06-22 05:46:44 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isdigit - checks for a digit
+ *@c: type integer
+ *Return: 1 if c is a digit, 0 if otherwise
+ */
+
+int _isdigit(int c)
+if (i >= 0 && i <= 9)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
