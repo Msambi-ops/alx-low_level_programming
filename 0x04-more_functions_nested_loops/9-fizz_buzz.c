e894c522708210be480e36fbf2deb1d1fd996cad
Msambi-ops <msambiroman@gmail.com>
2023-06-22 19:45:56 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - main block, solve fizz buzz from numbers 1 to 100
- * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
- * Multiples of both 3 and 5 should print FizzBuzz.
+ * main - main function of program
  * Return: 0
  */
 
