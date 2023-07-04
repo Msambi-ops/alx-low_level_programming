66f854b79081facb29c3abc7038fa84967028814
Msambi-ops <msambiroman@gmail.com>
2023-07-04 16:15:38 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include  "main.h"
+
+/**
+ *set_string - prints a value
+ *@s: char parameter
+ *@to: char parameter
+ *Return: 0 if successful
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
